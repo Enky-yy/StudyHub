30cceb9c90738b2ee030eb29900a05d1037e163f
Enky-yy <shahharsh4653@gmail.com>
2026-07-07 23:55:06 +0530

    Version 0.9.2
    Changes to be committed:
    	modified:   backend/main.py
    
    Changes not staged for commit:
    	modified:   frontend/studyhub/src/app/favicon.ico

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://study.harsh-shah.me",
-        "https://study-hub-two-eta.vercel.app"
+        "https://study-hub-two-eta.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
