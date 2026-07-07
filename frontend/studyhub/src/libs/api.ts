b9f4a4529b43bc9f26d3c2afe3e96a30b169c4ef
Enky-yy <shahharsh4653@gmail.com>
2026-07-07 23:16:11 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   frontend/studyhub/src/libs/api.ts

--- frontend/studyhub/src/libs/api.ts
+++ frontend/studyhub/src/libs/api.ts
@@ -1,15 +1,17 @@
-import axios from 'axios';
-import { getToken } from './auth';
+import axios from "axios";
+import { getToken } from "./auth";
 
-const api = axios.create({baseURL : 'http://127.0.0.1:8000'});
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000",
+});
 
-api.interceptors.request.use((config)=>{
-    const token = getToken();
+api.interceptors.request.use((config) => {
+  const token = getToken();
 
-    if (token){
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
